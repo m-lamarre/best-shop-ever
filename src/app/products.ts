fc609a0e38e2fab3fe36f545626040498428cb3f
m-lamarre <miriam@platform45.com>
2020-10-29 15:19:30 +0200

    first angular app ever

---
+++ src/app/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    name: "Phone XL",
+    price: 7999,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    name: "Phone Mini",
+    price: 6999,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    name: "Phone Standard",
+    price: 2999,
+    description: ""
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
